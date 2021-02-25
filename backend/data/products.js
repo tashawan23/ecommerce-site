77168ce36c32663aacacff2773ce8847f3745201
tashawan23 <tas.wan23@gmail.com>
2021-02-25 14:57:08 +0800

    First commit

---
+++ backend/data/products.js
@@ -0,0 +1,96 @@
+const products = [
+    {
+      name: "Smores Brownie",
+      imageUrl:
+        "images/Smores.PNG",
+      description:
+        "Our favourite campfire treat in a perfect oozing brownie",
+      price: 5,
+      countInStock: 15,
+    },
+    {
+      name: "Carrotcake Cupcakes",
+      imageUrl:
+        "images/CarrotCake.JPG",
+      description:
+        "Comforting moist and fluffy carrotcake cupcakes with toasted pecans and perfectly smooth cream cheese frosting",
+      price: 4,
+      countInStock: 10,
+    },
+    {
+      name: "Cookie Butter Brownie",
+      imageUrl:
+        "images/oozy.jpg",
+      description:
+        "Fluffy and rich chocolate brownie with a lava chocolate centre and cookie butter filling",
+      price: 5,
+      countInStock: 5,
+    },
+    {
+      name: "Cookie Butter Cupcakes",
+      imageUrl:
+        "images/cupcakes.PNG",
+      description:
+        "Soft and fluffy vanilla cupcake with a cookie butter cream filling and the best swiss meringue buttercream",
+      price: 4,
+      countInStock: 25,
+    },
+    {
+      name: "Chocolate Chip Cookie Dough Cupcake",
+      imageUrl:
+        "images/cookie.JPG",
+      description:
+        "Soft and fluffy chocolate chip cupcake with a cookie dough centre and cookie dough buttercream",
+      price: 4,
+      countInStock: 25,
+    },
+    {
+      name: "Cream Cheese Cookies & Cream",
+      imageUrl:
+        "images/cookiescream.JPG",
+      description:
+        "The best crunchy Oreo cookie with a light and fluffy cream cheese filling",
+      price: 5,
+      countInStock: 25,
+    },
+    {
+      name: "Salted Caramel Pretzel Cupcake",
+      imageUrl:
+        "images/pretzel2.JPG",
+      description:
+        "The best combination of sweet and salty! Soft vanilla cupcake with a salted caramel frosting and salted caramel drizzle",
+      price: 4,
+      countInStock: 25,
+    },
+    {
+      name: "Sea salt Brownie",
+      imageUrl:
+        "images/seasalt.jpg",
+      description:
+        "Rich and moist chocolate brownie with an oozing chocolate lava centre and topped with sea salt flakes ",
+      price: 5,
+      countInStock: 25,
+    },
+    {
+      name: "Dolce de leche Brownie",
+      imageUrl:
+        "images/dolce.jpg",
+      description:
+        "Rich and moist chocolate brownie with a milky and sweet dolce de leche centre ",
+      price: 5,
+      countInStock: 25,
+    },
+    {
+      name: "Cereal Milk Cupcake",
+      imageUrl:
+        "images/cereal.jpg",
+      description:
+        "Everyone's favourite cornflakes soaked in milk but in a cupcake! Vanilla cupcake with Cereal milk infused buttercream ",
+      price: 5,
+      countInStock: 25,
+    },
+    
+  ];
+  
+  module.exports = products;
+  //npm run data:import
